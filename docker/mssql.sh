347f3602cc6af003d68639238d070788215fa534
nghe.huavan <nghe.huavan@gmail.com>
2023-07-04 12:48:03 -0700

    .

--- docker/mssql.sh
+++ docker/mssql.sh
@@ -6,7 +6,7 @@ host_dir="/docker/persistent/sqlserver"
 inside_dir="/var/opt/mssql/data"
 env_var_0="ACCEPT_EULA=Y"
 env_var_1="SA_PASSWORD=Pwd123!@#"
-name="sqlserver-tag"
+name="$image-$tag"
 
-echo "-------------Run image with persistent [$host_dir] -------------"
+echo "-------------Run $image with persistent [$host_dir] -------------"
 docker run --name $name -v $host_dir:$inside_dir -e $env_var_0 -e $env_var_1 -p $port_0:$port_0 -d --restart always $image:$tag
--- docker/mysql.sh
+++ docker/mysql.sh
@@ -6,7 +6,7 @@ host_dir="/docker/persistent/mySql"
 inside_dir="/var/lib/mysql"
 env_var_0="MYSQL_ROOT_PASSWORD=Pwd123!@#"
 env_var_1=
-name="image-tag"
+name="$image-$tag"
 
 echo "-------------Run image with persistent [%host_dir] -------------"
 docker run --name $name -v $host_dir:$inside_dir -e $env_var_0 -p $port_0:$port_0 -d --restart always $image:$tag
