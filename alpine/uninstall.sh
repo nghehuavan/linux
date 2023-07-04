a73e89dbe40d3d6e71b1b38c163967869488aa0d
nghe.huavan <nghe.huavan@gmail.com>
2023-07-04 10:52:38 -0700

    .

--- alpine/uninstall.sh
+++ alpine/uninstall.sh
@@ -1,7 +1,9 @@
+## wget https://raw.githubusercontent.com/nghehuavan/linux/main/alpine/uninstall.sh
+## chmod +x uninstall.sh
+
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume prune -a -f
 service docker stop
 rc-update delete docker boot
 apk del docker docker-compose
-
