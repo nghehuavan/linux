3e0a6c4fd1db1327586731d5a21a451faf98cf07
nghe.huavan <nghe.huavan@gmail.com>
2023-07-04 12:14:02 -0700

    .

--- docker/redis.sh
+++ docker/redis.sh
@@ -1,3 +1,4 @@
+##https://hub.docker.com/_/redis
 set image=redis
 set tag=4.0
 set port_0=6379
@@ -6,4 +7,4 @@ set inside_dir=/data
 set env_var_0=
 set name=$image-$tag
 echo -------------Run $image with persistent [$host_dir] -------------
-docker run --name $name -v $host_dir:$inside_dir -p $port_0:$port_0 -d --restart always $image:$tag
+docker run --name "$name" -v "$host_dir:$inside_dir" -p "$port_0:$port_0" -d --restart always "$image:$tag"
