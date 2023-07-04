0e82e5ce427ba94b30ccccfd3ee8304a90376474
nghe.huavan <nghe.huavan@gmail.com>
2023-07-04 10:39:25 -0700

    .

--- alpine/install.sh
+++ alpine/install.sh
@@ -9,6 +9,7 @@ rc-update add docker boot
 addgroup root docker
 service docker start
 
+echo Waiting 30s for docker starting ...
 sleep 30
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
--- alpine/uninstall.sh
+++ alpine/uninstall.sh
@@ -1,3 +1,7 @@
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker volume prune -a -f
 service docker stop
-apk del docker docker-compose
 rc-update delete docker boot
+apk del docker docker-compose
+
