2825b257537fef4519445b002ce02ccbca7a5ea9
nghe.huavan <nghe.huavan@gmail.com>
2023-07-04 12:48:48 -0700

    .

--- docker/mssql.sh
+++ docker/mssql.sh
@@ -8,5 +8,5 @@ env_var_0="ACCEPT_EULA=Y"
 env_var_1="SA_PASSWORD=Pwd123!@#"
 name="$image-$tag"
 
-echo "-------------Run $image with persistent [$host_dir] -------------"
+echo -------------Run $image with persistent [$host_dir] -------------
 docker run --name $name -v $host_dir:$inside_dir -e $env_var_0 -e $env_var_1 -p $port_0:$port_0 -d --restart always $image:$tag
--- docker/mysql.sh
+++ docker/mysql.sh
@@ -8,5 +8,5 @@ env_var_0="MYSQL_ROOT_PASSWORD=Pwd123!@#"
 env_var_1=
 name="$image-$tag"
 
-echo "-------------Run image with persistent [%host_dir] -------------"
+echo -------------Run $image with persistent [$host_dir] -------------
 docker run --name $name -v $host_dir:$inside_dir -e $env_var_0 -p $port_0:$port_0 -d --restart always $image:$tag
